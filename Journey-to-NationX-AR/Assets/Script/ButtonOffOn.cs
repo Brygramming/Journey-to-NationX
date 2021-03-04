f7d65a9362b836c7bbecd4a18ce73457e0131189
Bryan Santiago <musicprostar11@gmail.com>
2021-03-03 21:05:15 -0500

    Button Test Successful

---
+++ Journey-to-NationX-AR/Assets/Script/ButtonOffOn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOffOn : MonoBehaviour
+{
+	public GameObject PartActive;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ParticleOffOn()
+	{
+		PartActive.SetActive(!PartActive.activeInHierarchy);
+	}
+}
