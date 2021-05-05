56d0699350ee318426683e8daf92493aef8f3ba4
Shanice Smith <shanicesmith.work@hotmail.com>
2021-05-05 19:50:36 -0400

    added dialogue controller

---
+++ Journey-to-NationX-AR/Assets/Script/DialogueController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+
+    public Text DialogueText;
+    public string[] Sentences;
+    private int Index = 0;
+    public float DialogueSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void NextSentence()
+    {
+        if (Index <= Sentences.Length - 1)
+        {
+            DialogueText.text = "";
+            StartCoroutine(WriteSentences());
+        }
+    }
+
+    IEnumerator WriteSentences()
+    {
+        foreach (char Character in Sentences[Index].ToCharArray())
+        {
+            DialogueText.text += Character;
+            yield return new WaitForSeconds(DialogueSpeed);
+        }
+        Index++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
