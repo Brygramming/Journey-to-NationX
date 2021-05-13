0a8c588ad09ec68baa8090835dc6f6b18307187d
Bryan Santiago <musicprostar11@gmail.com>
2021-05-13 02:40:32 -0400

    Dialogue works now, but is eiffy

--- Journey-to-NationX-AR/Assets/Script/DialogueController.cs
+++ Journey-to-NationX-AR/Assets/Script/DialogueController.cs
@@ -6,25 +6,31 @@ using TMPro;
 
 public class DialogueController : MonoBehaviour
 {
+    /*[Header("Canvas")]
+    public Canvas MainDialogue;
+    public Vector3 DialoguePosition;*/
 
+    [Header("Texts")]
     public Text DialogueText;
     public string[] Sentences;
     private int Index = 0;
+    [Range(0f, 1f)]
     public float DialogueSpeed;
-
 
     void Start()
     {
-        
+        //Canvas.ForceUpdateCanvases();
     }
 
     public void NextSentence()
     {
+        Index++;
         if (Index <= Sentences.Length - 1)
         {
             DialogueText.text = "";
             StartCoroutine(WriteSentences());
         }
+        //MoveCanvas();
     }
     public void PrevSentence()
     {
@@ -40,12 +46,23 @@ public class DialogueController : MonoBehaviour
             DialogueText.text += Character;
             yield return new WaitForSeconds(DialogueSpeed);
         }
-        Index++;
+        //Index++;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //To prevent array going out of range
+        if(Index < 0)
+        {
+            Index = 0;
+        }
     }
+
+    /*void MoveCanvas()
+    {
+        if(Index == 1)
+        {
+            MainDialogue.transform.position = new Vector3(0.5f, 0, 0);
+        }
+    }*/
 }
